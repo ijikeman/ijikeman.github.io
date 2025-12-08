bb108d75f1cc4cb45f6a6554854ef56ab1aae553
ijikeman <keiji.ue@gmail.com>
2025-12-08 09:52:16 +0900

    fixed

--- tcardgen.sh
+++ tcardgen.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
-MD_PATH=$1
-FONTDIR='HackGen_v2.9.0'
-MOUNT_POINT="/go/mount/"
-TEMPLATE_IMAGE='eyecatch_template.png'
-MOUNT_SRC='/home/vscode/project/ijikeman.github.io/'
 
+MD_PATH=$1 # hugo blog path
+FONTDIR='HackGen_v2.9.0'
+MOUNT_POINT="/go/mount/" # tcardgen mount point
+TEMPLATE_IMAGE='eyecatch_template.png' # eyecatch template file
+MOUNT_SRC='/home/vscode/project/ijikeman.github.io/' # hugo blog path
+TCARDGEN_IMAGE='tcardgen:v0.9.0'
+WEBP_IMAGE='webp:latest'
+
+# アイキャッチ画像フォルダの作成
 mkdir -p ./static/images/eyecatch/${MD_PATH}
 
-docker run --rm -v ${MOUNT_SRC}:${MOUNT_POINT} tcardgen bash -c \
+# tcardgenでアイキャッチ画像をpngで作成
+docker run --rm -v ${MOUNT_SRC}:${MOUNT_POINT} ${TCARDGEN_IMAGE} bash -c \
 "tcardgen --fontDir ${MOUNT_POINT}/static/fonts/${FONTDIR} \
 --output ${MOUNT_POINT}/static/images/eyecatch/${MD_PATH} \
 --template ${MOUNT_POINT}/static/images/templates/${TEMPLATE_IMAGE} --config ${MOUNT_POINT}/static/tcardgen/tcardgen.yaml \
 ${MOUNT_POINT}/content/posts/${MD_PATH}/index.md"
 
-docker run --rm -v ${MOUNT_SRC}:${MOUNT_POINT} tcardgen bash -c "convert -resize "50%" ${MOUNT_POINT}/static/images/eyecatch/${MD_PATH}/index.png ${MOUNT_POINT}/static/images/eyecatch/${MD_PATH}/index.dst.png"
-mv -f ./static/images/eyecatch/${MD_PATH}/index.dst.png ./static/images/eyecatch/${MD_PATH}/index.png
+# webpのcwebpコマンドでpng to webpコンバート
+docker run --rm -v ${MOUNT_SRC}:/${MOUNT_POINT} webp \
+cwebp ${MOUNT_POINT}/static/images/eyecatch/${MD_PATH}/index.png \
+-o ${MOUNT_POINT}/static/images/eyecatch/${MD_PATH}/index.webp
+
+# コンバートが終了したので削除
+rm -f ./static/images/eyecatch/${MD_PATH}/index.png
